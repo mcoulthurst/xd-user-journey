f3d68ad36b942dac3e796e9e2fcc441532f74da3
mcoulthurst <mc@lightness.co.uk>
2019-04-30 12:57:22 +0100

    Update manifest; clean up

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const { Line, Rectangle, Ellipse, Text, Color } = require("scenegraph");
 const CSV = require("./csv");
 const fs = require("uxp").storage.localFileSystem;
+var assets = require("assets");
+
 const textFill = ["grey-3", '#dee0e2'];
 const textFillDark = ["grey-1", '#6f777b'];
 const trafficLights = [
@@ -13,15 +15,6 @@ const trafficLights = [
     ["bright-red", '#df3034'],
     ["red", '#b10e1e']
 ];
-const rowColor = [
-    ["light-blue", '#2b8cc4'],
-    ["green (button)", '#00823b'],
-    ["bright-purple", '#912b88'],
-    ["light-purple", '#6f72af'],
-    ["orange", '#f47738'],
-    ["bright-red", '#df3034']
-];
-
 const palette = [
     ["Midnight Blue", '#2c3e50'],
     ["Belize Hole", '#2980b9'],
@@ -37,45 +30,17 @@ const ht = 100;
 const wd = 160;
 const gutterX = 5;
 const gutterY = 32;
+
 var fontSize = 12;
 var fontHeaderSize = 18;
 var fontLargeSize = 48;
 var offsetX = 2 * wd + gutterX;
 var offsetY = 110;
-var assets = require("assets");
 var allColors;
 
 
 
-function getAssetPalette(){
-    allColors = assets.colors.get();
-    if (allColors.length<4){
-        addPalette(palette);
-    }
-    //update the reference
-    allColors = assets.colors.get();
-}
-
-function addPalette(arr) {
-    var i = 0;
-    var len = arr.length;
-    for (i=0; i<len; i++){
-        addColor(arr[i])
-    }
-}
-
-
-function addColor(array) {
-    var assets = require("assets");
-    var label = array[0] + ' ('+array[1]+')';
-    var color = new Color(array[1]);
-    assets.colors.add([
-        { name: label, color: color }
-    ]);
-
-}
-
-async function insertTextFromFileHandler(selection) {
+async function createUserJourney(selection) {
     var i, j;
     // TODO get existing assets colours and use first four as row fills
 
@@ -88,65 +53,72 @@ async function insertTextFromFileHandler(selection) {
     const arr = CSV.parse(contents);
     offsetY = 110;
     getAssetPalette();
+    console.log(arr);
 
-    // draw side panel
-    drawSidePanel(arr, selection);
+    if(arr.length<6){
+        console.log('WARNING');
+    }else{
 
-    var rows = arr.length;
-    var cols;
-    var text;
-    for (j = 5; j < rows; j++) {
-        cols = arr[j].length;
-        // process the emotions with emoticons
-        if (j === 7) {
-            drawEmotions(arr[j], selection);
-        }
 
-        // double ht line for journey emotions
-        if (j === 8) {
-            offsetY = offsetY + ht;
-        }
+        // draw side panel
+        drawSidePanel(arr, selection);
 
-        for (i = 0; i < cols; i++) {
-            if (i == 0) {
-                // add header
-                var str = String(arr[j][i]).toUpperCase();
-                text = new Text();
-                text.text = str;
-                text.styleRanges = [{
-                    length: str.length,
-                    fill: new Color("black"),
-                    fontSize: fontHeaderSize
-                }];
-                selection.insertionParent.addChild(text);
-                text.moveInParentCoordinates(offsetX + (gutterX + (i) * (wd + gutterX)), offsetY - 2 * gutterX + (j - 4) * (ht + gutterY));
+        var rows = arr.length;
+        var cols;
+        var text;
+        for (j = 5; j < rows; j++) {
+            cols = arr[j].length;
+            // process the emotions with emoticons
+            if (j === 7) {
+                drawEmotions(arr[j], selection);
             }
 
-            if (arr[j][i] !== null && i > 0 && j !== 7) {
-                var str = String(arr[j][i]); // cast to string so we can get length
-                text = new Text();
-                text.areaBox = { width: wd - gutterX * 3, height: ht };
-                text.text = str;
-                text.styleRanges = [{
-                    length: str.length,
-                    fill: new Color(textFill[1]),
-                    fontSize: fontSize
-                }];
-
-                const rect = new Rectangle();
-                rect.width = wd;
-                rect.height = ht;
-                rect.fill = new Color(allColors[j - 5].color);
-                rect.stroke = null;
-
-                selection.insertionParent.addChild(rect);
-                rect.moveInParentCoordinates(offsetX + ((i - 1) * (wd + gutterX)), (offsetY + (j - 4) * (ht + gutterY)));
-
-                selection.insertionParent.addChild(text);
-                text.moveInParentCoordinates(offsetX + (gutterX + (i - 1) * (wd + gutterX)), offsetY  + (j - 4) * (ht + gutterY) + 2* gutterX);
-
+            // double ht line for journey emotions
+            if (j === 8) {
+                offsetY = offsetY + ht;
             }
 
+            for (i = 0; i < cols; i++) {
+                if (i == 0) {
+                    // add header
+                    var str = String(arr[j][i]).toUpperCase();
+                    text = new Text();
+                    text.text = str;
+                    text.styleRanges = [{
+                        length: str.length,
+                        fill: new Color("black"),
+                        fontSize: fontHeaderSize
+                    }];
+                    selection.insertionParent.addChild(text);
+                    text.moveInParentCoordinates(offsetX + (gutterX + (i) * (wd + gutterX)), offsetY - 2 * gutterX + (j - 4) * (ht + gutterY));
+                }
+
+                if (arr[j][i] !== null && i > 0 && j !== 7) {
+                    var str = String(arr[j][i]); // cast to string so we can get length
+                    text = new Text();
+                    text.areaBox = { width: wd - gutterX * 3, height: ht };
+                    text.text = str;
+                    text.styleRanges = [{
+                        length: str.length,
+                        fill: new Color(textFill[1]),
+                        fontSize: fontSize
+                    }];
+
+                    const rect = new Rectangle();
+                    rect.width = wd;
+                    rect.height = ht;
+                    console.log(j-5, allColors[j-5]);
+                    rect.fill = new Color(allColors[j - 5].color);
+                    rect.stroke = null;
+
+                    selection.insertionParent.addChild(rect);
+                    rect.moveInParentCoordinates(offsetX + ((i - 1) * (wd + gutterX)), (offsetY + (j - 4) * (ht + gutterY)));
+
+                    selection.insertionParent.addChild(text);
+                    text.moveInParentCoordinates(offsetX + (gutterX + (i - 1) * (wd + gutterX)), offsetY  + (j - 4) * (ht + gutterY) + 2* gutterX);
+
+                }
+            }
         }
     }
 }
@@ -179,25 +151,22 @@ function drawEmotions(arr, selection) {
             line.strokeWidth = 3;
 
             selection.insertionParent.addChild(line);
-            //selection.editContext.addChild(line);
         }
+
         lastX = x;
         lastY = y;
     }
-
 
     for (i = 1; i < len; i++) {
         const value = parseInt(arr[i]);
         x = offsetX + ((i - 1) * (wd + gutterX)) + wd / 2 - wd / 5;
         y = (offsetY + (3) * (ht + gutterY)) + value * ht / 5;
         const circ = new Ellipse();
-
         circ.radiusX = wd / 5;
         circ.radiusY = wd / 5;
         circ.fill = new Color(trafficLights[value][1]);
         circ.stroke = new Color('white');
         circ.strokeWidth = 10;
-
 
         selection.insertionParent.addChild(circ);
         circ.moveInParentCoordinates(x, y);
@@ -256,8 +225,7 @@ function drawSidePanel(arr, selection) {
                 text.styleRanges = [{
                     length: str.length,
                     fill: new Color(textFill[1]),
-                    fontSize: displayFont,
-                    //fontStyle: 'bold'
+                    fontSize: displayFont
                 }];
 
                 selection.insertionParent.addChild(text);
@@ -270,8 +238,36 @@ function drawSidePanel(arr, selection) {
 
 }
 
+// palette utilities
+function getAssetPalette(){
+    allColors = assets.colors.get();
+    if (allColors.length<4){
+        addPalette(palette);
+        //update the reference
+        allColors = assets.colors.get();
+    }
+}
+
+function addPalette(arr) {
+    var i = 0;
+    var len = arr.length;
+    for (i=0; i<len; i++){
+        addColor(arr[i])
+    }
+}
+
+function addColor(array) {
+    var assets = require("assets");
+    var label = array[0] + ' ('+array[1]+')';
+    var color = new Color(array[1]);
+    assets.colors.add([
+        { name: label, color: color }
+    ]);
+
+}
+
 module.exports = {
     commands: {
-        "insertTextFromFileCommand": insertTextFromFileHandler
+        "createUserJourney": createUserJourney
     }
 };
