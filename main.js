ef32518ee72ae68ce91553dfcef2d9fe26f90990
mcoulthurst <mc@lightness.co.uk>
2019-05-07 16:10:21 +0100

    Change color palette, draw rows

--- main.js
+++ main.js
@@ -5,64 +5,47 @@ const CSV = require("./lib/csv");
 const fs = require("uxp").storage.localFileSystem;
 var assets = require("assets");
 
-const textFill = ["grey-3", '#dee0e2'];
-const textFillDark = ["grey-1", '#6f777b'];
-const trafficLights = [
-    ["green", '#006435'],
-    ["green (button)", '#00823b'],
-    ["light-green", '#85994b'],
-    ["yellow", '#ffbf47'],
-    ["orange", '#f47738'],
-    ["bright-red", '#df3034'],
-    ["red", '#b10e1e']
-];
+
 const palette = [
-    ["Midnight Blue", '#2c3e50'],
-    ["Belize Hole", '#2980b9'],
-    ["Wisteria", '#8e44ad'],
-    ["Pumpkin", '#d35400'],
-    ["Pomegranate", '#c0391b'],
-    ["grey-4", '#f8f8f8'],
-    ["grey-3", '#dee0e2'],
-    ["grey-2", '#bfc1c3'],
-    ["grey-1", '#6f777b'],
-    ["black", '#0b0c0c'],
-    ["blue", '#005ea5'],
-    ["light-blue", '#2b8cc4'],
-    ["turquoise", '#28a197'],
-    ["green", '#006435'],
-    ["green button", '#00823b'],
-    ["light-green", '#85994b'],
-    ["yellow", '#ffbf47'],
-    ["brown", '#b58840'],
-    ["orange", '#f47738'],
-    ["bright-red", '#df3034'],
-    ["red", '#b10e1e'],
-    ["light-pink", '#f499be'],
-    ["pink", '#d53880'],
-    ["bright-purple", '#912b88'],
-    ["light-purple", '#6f72af'],
-    ["purple", '#2e358b'],
-    ["deep purple", '#4c2c92']
+    ["Side Panel", '#6F777B'],
+    ["Side Text", '#DEE0E2'],
+    ["Tasks", '#A7488C'],
+    ["Personas", '#35C0BD'],
+    ["Touch Points", '#2C3E50'],
+    ["Pain Points", '#F15159'],
+    ["Dark Text", '#4C4743']
 ];
+
+const sidePanelFill = palette[0][1];
+const sidePanelText = palette[1][1];
+const titleText = '#ffffff';
+const defaultText = palette[6][1];
+
+
+const ht_row = 170;
+const wd_row = 30;
+const wd_full = 1920;
+const wd_offset = 330;
+const ht_offset = 190;
 
 const ht = 100;
 const wd = 160;
+const gutter = 10;
 const gutterX = 5;
 const gutterY = 32;
+
+var offsetX = 2 * wd + gutterX;
+var offsetY = 110;
 
 var fontSize = 12;
 var fontHeaderSize = 18;
 var fontLargeSize = 48;
-var offsetX = 2 * wd + gutterX;
-var offsetY = 110;
 var allColors;
 
 
 
 async function createUserJourney(selection) {
     var i, j;
-    // TODO get existing assets colours and use first four as row fills
 
     const aFile = await fs.getFileForOpening({ types: ["txt", "csv"] });
     if (!aFile) {
@@ -72,82 +55,127 @@ async function createUserJourney(selection) {
     const contents = await aFile.read();
     const arr = CSV.parse(contents);
     offsetY = 110;
-    getAssetPalette();
+
 
     if(arr.length<6 ){
         showError('Not enough Data');
     }else if(arr.length>20 ){
         showError('Too Many Rows');
     }else{
+        drawSidePanel(arr.slice(0,5), selection);
 
+        drawJourney(arr.slice(5), selection);
+    }
+}
 
-        // draw side panel
-        drawSidePanel(arr, selection);
+function drawJourney(arr, selection) {
+    console.log(arr);
+    var rows = arr.length;
+    var cols;
+    var text;
+    var i,j;
 
-        var rows = arr.length;
-        var cols;
-        var text;
-        for (j = 5; j < rows; j++) {
-            cols = arr[j].length;
-            // process the emotions with emoticons
-            if (j === 7) {
-                drawEmotions(arr[j], selection);
+    // draw background rows and title blocks
+    var str = " ";
+    for (j = 0; j < rows; j++) {
+        // draw row
+        var rect = new Rectangle();
+        rect.width = wd_full;
+        rect.height = ht_row;
+        rect.fill = new Color(palette[j+2][1], 0.1);
+        rect.stroke = null;
+        selection.insertionParent.addChild(rect);
+        rect.moveInParentCoordinates(wd_offset + gutter + wd_row, (ht_offset + j * (ht_row + gutter) ) );
+
+        // draw header
+        var rect = new Rectangle();
+        rect.width = wd_row;
+        rect.height = ht_row;
+        rect.fill = new Color(palette[j+2][1]);
+        rect.stroke = null;
+        selection.insertionParent.addChild(rect);
+        rect.moveInParentCoordinates(wd_offset, (ht_offset + j * (ht_row + gutter)));
+
+        // text
+
+        if (arr[j][0] !== null && arr[j][0] !== ""){
+            str = String(arr[j][0]);
+        }
+        text = new Text();
+        text.text = str;
+        text.styleRanges = [{
+            length: str.length,
+            fill: new Color(titleText),
+            fontSize: fontHeaderSize
+        }];
+        //text.rotation = 90;
+        selection.insertionParent.addChild(text);
+        text.moveInParentCoordinates(wd_offset + wd_row/2, (ht_offset + j * (ht_row + gutter)) + ht_row/2);
+
+    }
+
+    /*
+    for (j = 0; j < rows; j++) {
+        cols = arr[j].length;
+        // process the emotions with emoticons
+        if (j === 7) {
+            drawEmotions(arr[j], selection);
+        }
+
+        // double ht line for journey emotions
+        if (j === 8) {
+            offsetY = offsetY + ht;
+        }
+
+        var str = " ";
+        for (i = 0; i < cols; i++) {
+            if (i == 0) {
+                // add header
+                if (arr[j][i] !== null && arr[j][i] !== ""){
+                    str = String(arr[j][i]);
+                }
+                text = new Text();
+                text.text = str;
+                text.styleRanges = [{
+                    length: str.length,
+                    fill: new Color("black"),
+                    fontSize: fontHeaderSize
+                }];
+                selection.insertionParent.addChild(text);
+                text.moveInParentCoordinates(offsetX + (gutterX + (i) * (wd + gutterX)), offsetY - 2 * gutterX + (j - 4) * (ht + gutterY));
             }
 
-            // double ht line for journey emotions
-            if (j === 8) {
-                offsetY = offsetY + ht;
-            }
-
-            var str = " ";
-            for (i = 0; i < cols; i++) {
-                if (i == 0) {
-                    // add header
-                    if (arr[j][i] !== null && arr[j][i] !== ""){
-                        str = String(arr[j][i]);
-                    }
+            if (arr[j][i] !== null && i > 0 && j !== 7) {
+                if (arr[j][i] !== "") {
+                    var str = String(arr[j][i]); // cast to string so we can get length
                     text = new Text();
+                    text.areaBox = { width: wd - gutterX * 3, height: ht };
                     text.text = str;
                     text.styleRanges = [{
                         length: str.length,
-                        fill: new Color("black"),
-                        fontSize: fontHeaderSize
+                        fill: new Color(textFill[1]),
+                        fontSize: fontSize
                     }];
-                    selection.insertionParent.addChild(text);
-                    text.moveInParentCoordinates(offsetX + (gutterX + (i) * (wd + gutterX)), offsetY - 2 * gutterX + (j - 4) * (ht + gutterY));
                 }
 
-                if (arr[j][i] !== null && i > 0 && j !== 7) {
-                    if (arr[j][i] !== "") {
-                        var str = String(arr[j][i]); // cast to string so we can get length
-                        text = new Text();
-                        text.areaBox = { width: wd - gutterX * 3, height: ht };
-                        text.text = str;
-                        text.styleRanges = [{
-                            length: str.length,
-                            fill: new Color(textFill[1]),
-                            fontSize: fontSize
-                        }];
-                    }
+                const rect = new Rectangle();
+                rect.width = wd;
+                rect.height = ht;
+                rect.fill = new Color(allColors[j - 5].color);
+                rect.stroke = null;
 
-                    const rect = new Rectangle();
-                    rect.width = wd;
-                    rect.height = ht;
-                    rect.fill = new Color(allColors[j - 5].color);
-                    rect.stroke = null;
+                selection.insertionParent.addChild(rect);
+                rect.moveInParentCoordinates(offsetX + ((i - 1) * (wd + gutterX)), (offsetY + (j - 4) * (ht + gutterY)));
 
-                    selection.insertionParent.addChild(rect);
-                    rect.moveInParentCoordinates(offsetX + ((i - 1) * (wd + gutterX)), (offsetY + (j - 4) * (ht + gutterY)));
-
-                    if (arr[j][i] !== "") {
-                        selection.insertionParent.addChild(text);
-                        text.moveInParentCoordinates(offsetX + (gutterX + (i - 1) * (wd + gutterX)), offsetY  + (j - 4) * (ht + gutterY) + 2* gutterX);
-                    }
+                if (arr[j][i] !== "") {
+                    selection.insertionParent.addChild(text);
+                    text.moveInParentCoordinates(offsetX + (gutterX + (i - 1) * (wd + gutterX)), offsetY  + (j - 4) * (ht + gutterY) + 2* gutterX);
                 }
             }
         }
-    }
+    }*/
 }
+
 
 // do two loops one to set line beneath all circles
 function drawEmotions(arr, selection) {
@@ -212,11 +240,12 @@ function drawEmotions(arr, selection) {
 
 
 function drawSidePanel(arr, selection) {
+    console.log("-------------------------");
     const len = 5; // get first four rows to use as side-bar content
     const rect = new Rectangle();
     rect.width = wd * 2;
     rect.height = 1080;
-    rect.fill = new Color("#6f777b");
+    rect.fill = new Color(sidePanelFill);
     rect.stroke = null;
     rect.opacity = 1;
     selection.insertionParent.addChild(rect);
@@ -225,7 +254,7 @@ function drawSidePanel(arr, selection) {
     circ.radiusX = wd / 2 - gutterX;
     circ.radiusY = wd / 2 - gutterX;
     circ.fill = null;
-    circ.stroke = new Color('#dee0e2');
+    circ.stroke = new Color(sidePanelText);
     circ.strokeWidth = 3;
     selection.insertionParent.addChild(circ);
     circ.moveInParentCoordinates(gutterX + wd / 2, gutterY);
@@ -239,16 +268,20 @@ function drawSidePanel(arr, selection) {
     text.text = str;
     text.styleRanges = [{
         length: str.length,
-        fill: new Color(textFillDark[1]),
+        fill: new Color(defaultText),
         fontSize: fontLargeSize
     }];
 
     selection.insertionParent.addChild(text);
     text.moveInParentCoordinates((offsetX + gutterX), (2*gutterY + ht / 2));
-    var i, j, displayFont;
 
+    var i, j, displayFont;
+    var rowLength = 0;
     for (j = 1; j < len; j++) {
-        for (i = 0; i < len; i++) {
+        console.log("row" + arr[j]);
+        rowLength = arr[j].length;
+        for (i = 0; i < rowLength; i++) {
+            console.log(arr[j][i]);
             // TODO: loop thru items and built a bullet list
             // add as an areabox.
             // get height?
@@ -263,7 +296,7 @@ function drawSidePanel(arr, selection) {
                 text.text = str;
                 text.styleRanges = [{
                     length: str.length,
-                    fill: new Color(textFill[1]),
+                    fill: new Color(sidePanelText),
                     fontSize: displayFont
                 }];
 
@@ -287,33 +320,6 @@ async function showError(header) {
         "* See the plugin help page for more information");}
 
 
-// palette utilities
-function getAssetPalette(){
-    allColors = assets.colors.get();
-    if (allColors.length<4){
-        addPalette(palette);
-        //update the reference
-        allColors = assets.colors.get();
-    }
-}
-
-function addPalette(arr) {
-    var i = 0;
-    var len = arr.length;
-    for (i=0; i<len; i++){
-        addColor(arr[i])
-    }
-}
-
-function addColor(array) {
-    var assets = require("assets");
-    var label = array[0] + ' ('+array[1]+')';
-    var color = new Color(array[1]);
-    assets.colors.add([
-        { name: label, color: color }
-    ]);
-
-}
 
 module.exports = {
     commands: {
